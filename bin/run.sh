dd841315c7896f97c189669291953bf1ef5d510a
SittisukC1 <sittisukc@cariva.co.th>
2023-09-26 13:58:59 +0700

    update

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,10 @@
-MyVideos=("USA" "atom" "awesome-face")
-PS3="Please select a video card: "
-select myvideo in ${MyVideos[@]}
+listItems=("USA" "atom" "awesome-face")
+PS3="Please select a templates: "
+select itemSelected in ${listItems[@]}
 do
-    echo "$myvideo selected"
-    npx github:piuccio/cowsay cowthink -f $myvideo
+    echo "$itemSelected selected"
+    npx github:piuccio/cowsay "" -f $itemSelected
+
+    npx react-native init name --template https://sittisuk:glpat-hVgdL59nWmpgv4NWTYND@gitlab.com/bigbanggroups/cariva/carivakit/webLibTemplate.git
     break
 done
